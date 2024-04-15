bb2b950b3a2ad1ba77e55649bf05908687c66d25
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2024-04-15 21:47:00 +0530

    created routes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/authRoute.js";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 7000
+
+// middlewares
+app.use("/api/users", authRoute);
 
 
 // DB connecting
