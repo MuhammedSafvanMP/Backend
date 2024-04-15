ef1e0b6b7b9bba2ce4727482aa33a1571ceeb5f2
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2024-04-15 21:36:02 +0530

    mongoDB Atlas connected

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 7000
+
+
+// DB connecting
+mongoose.connect(process.env.DB)
+.then(() => console.log("DB connected"))
+.catch(error => console.log(error));
+
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
