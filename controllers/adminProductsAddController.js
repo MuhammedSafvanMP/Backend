4bee3fe348a882722fc1116bb28b71f8c547d137
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2024-04-17 20:32:31 +0530

    product joi validation added

--- controllers/adminProductsAddController.js
+++ controllers/adminProductsAddController.js
@@ -1,27 +1,34 @@
 
 import Products from "../models/productsModel.js";
+import porductJoi from "../validation/productJoi.js";
 
 export const createProducts = async (req, res, next) => {
     try {
-        const { title, description, price, category } = req.body;
+        // const { title, description, price, category } = req.body;
 
         // Check if required fields are provided
-        if (!title || !description || !price || !category) {
-            return res.status(400).json({ message: "Bad request" });
-        }
+        // if (!title || !description || !price || !category) {
+        //     return res.status(400).json({ message: "Bad request" });
+        // }
+        
+        // validate request body using Joi
+        const result = await porductJoi.validateAsync(req.body)
 
         const newProduct = new Products({
-            title,
-            description,
-            price,
-            category,
+            title: result.title,
+            description: result.description,
+            price: result.price,
+            category: result.category,
             productImg: req.file ? req.file.filename : null 
         });
 
         await newProduct.save();
         return res.status(201).json({ message: "Product created successfully" });
     } catch (error) {
-        console.log(error)
+        // Handle Joi validation errors
+        if (error.isJoi === true) {
+            return res.status(422).json({ message: "Validation Error", details: error.details });
+          }
         next(error);
     }
 };
---
+++ validation/productJoi.js
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+// Product Joi validation schema
+const productJoi = Joi.object({
+    title: Joi.string().min(3).max(30).required(),
+    description: Joi.string().min(3).max(50).required(),
+    price: Joi.number().integer().positive(),
+    category: Joi.string().min(3).max(30).required()
+});
+
+export default productJoi;
