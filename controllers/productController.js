892557db88a2bb7126175ea365aa6612ac64078f
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2024-04-17 11:41:19 +0530

    get all products jwt cookie token checked

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,31 @@
 import Products from "../models/productsModel";
 import { errorHandler } from "../error/error";
+import jwt from "jsonwebtoken";
 
 export const allProducts = async (req, res, next) => {
     try {
-        Products.find()
-        .then((products) => {
-            return res.status(200).json({products: products})
-        })
-        .catch((error) => {
-            return next(errorHandler(500, "Unable to get products", error));
+        // Check if the request contains a JWT token
+        const token = req.cookies.access_token;
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized - No token provided" });
+        }
 
-        })
+        // Verify the JWT token
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: "Unauthorized - Invalid token" });
+            }
+
+            // Token is valid, user is authenticated
+            Products.find()
+                .then((products) => {
+                    return res.status(200).json({ products });
+                })
+                .catch((error) => {
+                    return next(errorHandler(500, "Unable to get products", error));
+                });
+        });
     } catch (error) {
         return next(errorHandler(500, "Unable to get products", error));
     }
-}
+};
