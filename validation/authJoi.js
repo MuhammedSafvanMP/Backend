0965c724347d9f55c4bd275a3fd1160e65375e94
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2024-05-01 03:27:13 +0530

    modify code again

--- validation/authJoi.js
+++ validation/authJoi.js
@@ -9,8 +9,8 @@ const authJoi = Joi.object({
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net"] },
-  }).lowercase().required(),
-  password: Joi.string().min(8).required().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  }).lowercase().required().trim(),
+  password: Joi.string().min(8).required().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).trim(),
 
 });
 
